537e7e75681b5ed63a415a0876bde5f35b5ff45b
DavidCardonaG <softwarewd@outlook.com>
2023-07-18 00:22:06 -0500

    Fourth-Class

--- game/components/enemies/enemy_manager.py
+++ game/components/enemies/enemy_manager.py
@@ -1,19 +1,30 @@
+from random import randint
 from game.components.enemies.enemy import Enemy
+
 
 class EnemyManager:
     def __init__(self):
         self.enemies = []
-
-    def update(self):
+        
+    def update(self, game):
         self.add_enemy()
-
+        
         for enemy in self.enemies:
-            enemy.update(self.enemies)
-
-    def draw(self,screen):
+            enemy.update(self.enemies, game)
+    
+    def draw(self, screen):
         for enemy in self.enemies:
             enemy.draw(screen)
+            
     def add_enemy(self):
-        if len(self.enemies) < 6:
+        enemy_type = randint(1, 2)
+        if enemy_type == 1:
             enemy = Enemy()
+        else:
+            speed_x = 5
+            speed_y = 2
+            move_x_for = [50, 120]
+            enemy = Enemy(enemy_type, speed_x, speed_y, move_x_for)
+            
+        if len(self.enemies) < 1:
             self.enemies.append(enemy)
